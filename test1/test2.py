aa31887d613c8d9bdd6ca408a0097df6f8201249
xuchaoao <xuchaoao@outlook.com>
2019-04-09 22:29:41 +0800

    1

--- test1/test2.py
+++ test1/test2.py
@@ -2,3 +2,11 @@
 # -*- coding: utf-8 -*-
 # Author: Archerx
 # @time: 2019/4/9 下午 08:39
+
+import logging
+
+# logging.debug('aaaaa')
+# logging.warning('adasdsdss')
+
+import os
+print(os.path.expanduser('~'))
